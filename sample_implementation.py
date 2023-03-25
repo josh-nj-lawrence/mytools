df35c66d247be5a9929d987c6e3fed5d42023d7b
Josh Lawrence <Josh.lawrence@thesmartcone.com>
2023-03-25 18:51:23 -0400

    Initial Commit

---
+++ sample_implementation.py
@@ -0,0 +1,18 @@
+from timing import *
+
+### Sample implementation of different Tools
+@timeit_avg(15)
+def pass_func(n):
+    for i in range(n):
+        pass
+
+@timeit
+def if_func(n):
+    for i in range(n):
+        if i == n-1:
+            return "Done"
+
+
+if __name__ == "__main__":
+    pass_func(1_000_000)
+    if_func(1_000_000)
---
+++ timing.py
@@ -0,0 +1,34 @@
+from time import time as perf_counter
+import random
+
+# Timing wrapper
+def timeit(method):
+    def wrapper(*arg, **kw):
+        t = perf_counter()
+        ret = method(*arg, **kw)
+        print('Method ' + method.__name__ + ' took : ' +
+              "{:2.5f}".format(perf_counter()-t) + ' sec')
+        return ret
+    return wrapper
+
+# Average Timing wrapper
+def timeit_avg(itterations: int =10):
+    def deco(method):
+        def wrapper(*args, **kw):
+            results = [0]*itterations
+            for i in range(itterations):
+                t = perf_counter()
+                ret = method(*args, **kw)
+                results[i] = perf_counter() - t
+            print('Method ' + method.__name__ + ' took : ' +
+                  '{:2.5f}'.format(sum(results)/itterations) + 
+                  ' sec on average over {} itterations'.format(itterations))
+            return ret
+        return wrapper
+    return deco
+
+if __name__ == "__main__":
+    @timeit
+    def test_method():
+        print("Test")
+    test_method()
